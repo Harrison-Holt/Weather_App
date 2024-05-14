b94c53e550994074a10ec93e657752edf42502c7
Harrison-Holt <hholt2901@gmail.com>
2024-05-14 15:42:38 -0400

    Fixed the Homepage link

--- weather-app/src/UI/Header.js
+++ weather-app/src/UI/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <header>
             <h1>Weather App</h1>
-            <link href='https://personal-blog-website-kappa.vercel.app/'>Homepage</link>
+            <a href='https://personal-blog-website-kappa.vercel.app/'>Homepage</a>
         </header>
     ); 
 }; 
