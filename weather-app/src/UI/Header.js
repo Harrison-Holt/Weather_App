02ed114e3dda4996bb9e5c88a90fd370301f2c6e
Harrison-Holt <hholt2901@gmail.com>
2024-05-14 15:38:33 -0400

    Added the Homepage link

--- weather-app/src/UI/Header.js
+++ weather-app/src/UI/Header.js
@@ -5,6 +5,7 @@ function Header() {
     return (
         <header>
             <h1>Weather App</h1>
+            <link href='https://personal-blog-website-kappa.vercel.app/'>Homepage</link>
         </header>
     ); 
 }; 
