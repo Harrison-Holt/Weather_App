b9308ee069010f755799efea12887c254de08543
Harrison Holt <116909429+Harrison-Holt@users.noreply.github.com>
2024-08-12 14:02:44 -0400

    Update Footer.js

--- weather-app/src/UI/Footer.js
+++ weather-app/src/UI/Footer.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => (
-  <footer>
-    <p>&copy; 2024 My Weather App. All rights reserved.</p>
-  </footer>
+   <footer style={footerStyles}>
+            <h1 style={headerStyle}>Let's Connect!</h1>
+            <a href="https://github.com/Harrison-Holt" className="icon-link" aria-label="Visit Harrison's GitHub profile" title="Visit Harrison's GitHub profile">
+                <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/harrison-holt-18a703202" className="icon-link" aria-label="Visit Harrison's LinkedIn profile" title="Visit Harrison's LinkedIn profile">
+                <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="mailto:hholt2901@gmail.com" className="icon-link email-icon" aria-label="Send an email to Harrison" title="Send an email to Harrison">
+                <i className="fa fa-envelope"></i>
+            </a>
+        </footer>
 );
 
 export default Footer;
